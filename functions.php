5e3e2f6dbcd9de419ff4c20c51593fae1ecfd39b
root <root@slice42.dealertrend.com>
2011-08-23 11:10:34 -0700

    First draft of the theme object.

--- functions.php
+++ functions.php
@@ -1,22 +1,51 @@
 <?php
 
-function genesis_failed_to_load() {
-	echo '<div id="message" class="error"><p>Unable to locate the Genesis Framework!</p></div>';
+namespace WordPress\Themes\Genesis\Enthusiast;
+
+class Theme {
+
+	private $genesis_engine = NULL; 
+
+	function __construct() {
+		$this->configure_theme();
+		$this->start_genesis_framework();
+	}
+
+	function configure_theme() {
+		$this->genesis_engine = TEMPLATEPATH . '/lib/init.php';
+		add_custom_background();
+		add_theme_support( 'genesis-custom-header', array( 'width' => 960, 'height' => 80 ) );
+		add_theme_support( 'genesis-footer-widgets', 3 );
+	}
+
+	function start_genesis_framework() {
+		if( file_exists ( $this->genesis_engine ) ) {
+			require_once( $this->genesis_engine );
+		} else {
+			$this->failed_to_start();
+		}
+	}
+
+	function failed_to_start() {
+		add_action( 'admin_notices' , array( &$this , 'could_not_find_genesis' ) );
+		add_action( 'init' , array( &$this, 'deactivate_theme' ) );
+	}
+
+	function deactivate_theme() {
+		switch_theme( WP_DEFAULT_THEME , WP_DEFAULT_THEME );
+	}
+
+	function could_not_find_genesis() {
+		echo '<div id="message" class="error">';
+		echo '<p>Unable to locate the Genesis Framework!</p>';
+		echo '<p id="message" class="error">Deactivating Enthusiast.</p>';
+		echo '</div>';
+	}
+
 }
 
-/** Start the engine */
-if( file_exists( TEMPLATEPATH . '/lib/init.php' ) ) { 
-	require_once( TEMPLATEPATH . '/lib/init.php' );
-} else {
-	add_action( 'admin_notices' , 'genesis_failed_to_load' );
-	return false;
-}
+global $Enthusiast;
 
-/** Add support for custom background */
-add_custom_background();
+$Enthusiast = new Theme();
 
-/** Add support for custom header */
-add_theme_support( 'genesis-custom-header', array( 'width' => 960, 'height' => 80 ) );
-
-/** Add support for 3-column footer widgets */
-add_theme_support( 'genesis-footer-widgets', 3 );
+?>
